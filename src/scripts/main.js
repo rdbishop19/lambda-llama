1e84e6570ad7b42a0b933e4c5c5b886a73496377
Ryan Bishop <rdbishop06@gmail.com>
2019-10-15 11:24:51 -0500

    Return function within function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 console.log("Alpaca bag!");
+
+/* 
+Not only can functions take other functions as arguments, but functions can also 
+return functions! This is marked as an advanced challenge because 
+you haven't been introduced to this concept yet.
+
+The llamaNamer function defines an internal function and your job is to rewrite this code so that the internal function gets returned.
+ */
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function () {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
+        return name + suffix
+    }
+
+    return namer();
+}
+
+nameMaker = llamaNamer()
+console.log(nameMaker);
